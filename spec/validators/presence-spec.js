c6cbec8bdaefa35f21bd8ffa7b6051cc82700a7e
Pauli Price <pauli.price@gmail.com>
2013-12-07 21:34:51 -0500

    functions are first class objects - allow presence to ‘see’ them

--- spec/validators/presence-spec.js
+++ spec/validators/presence-spec.js
@@ -16,6 +16,7 @@ describe('validator.presence', function() {
     expect(presence(false, {})).not.toBeDefined();
     expect(presence([null], {})).not.toBeDefined();
     expect(presence({foo: null}, {})).not.toBeDefined();
+    expect(presence({foo: function(){return null;}}, {})).not.toBeDefined();
   });
 
   it("has a nice default message", function() {
