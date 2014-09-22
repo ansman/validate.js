a15d8c60536837db12088639d3882888e9e3b78d
Nicklas Ansman Giertz <nicklas@ansman.se>
2014-09-22 20:49:34 +0200

    Update the copyright

--- validate.js
+++ validate.js
@@ -1,5 +1,5 @@
 //     Validate.js 0.3.0
-//     (c) 2014 Wrapp
+//     (c) 2013-2014 Nicklas Ansman, Wrapp
 //     Validate.js may be freely distributed under the MIT license.
 //     For all details and documentation:
 //     http://validatejs.org/
