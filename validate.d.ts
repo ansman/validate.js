62ac4a492e262c437d3aaf61c24aa3353a9e877e
Jeff Barnes <jbarnes@gci.com>
2016-11-06 07:08:16 -0900

    Fix typescript noImplicitAny.

--- validate.d.ts
+++ validate.d.ts
@@ -1,6 +1,6 @@
 export declare interface ValidateJS {
   (attributes: any, constraints: any, options?: any): any;
-  async(attributes, constraints, options?: any): Promise<any>;
+  async(attributes: any, constraints: any, options?: any): Promise<any>;
   single(value: any, constraints: any, options?: any): any;
 }
 
